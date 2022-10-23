7248a3289366e379e9fd9ceb8442c015477939eb
Alexandr Kuzmich <92593750+xofmdo@users.noreply.github.com>
2022-10-23 20:52:38 +0300

    настройка проекта

---
+++ api_yamdb/api/validators.py
@@ -0,0 +1,29 @@
+from datetime import datetime as dt
+
+from rest_framework import exceptions
+
+from django.core.exceptions import ValidationError
+
+
+class MyUsernameValidator(object):
+
+    def __call__(self, value):
+        username = value.get('username')
+        if username == 'me':
+            raise exceptions.ValidationError(
+                {
+                    'username':
+                        ['Нельзя создать пользователя с username = me.', ]
+                }
+            )
+        return value
+
+
+def validate_year(value):
+    current_year = dt.now().year
+    if value > current_year:
+        raise ValidationError(
+            f'Год произведения не может быть больше, чем {current_year}!'
+            f'Проверьте указанный год!'
+        )
+    return True
---
+++ api_yamdb/reviews/admin.py
@@ -0,0 +1,32 @@
+from django.contrib import admin
+from reviews.models import Genre, Title
+
+
+@admin.register(Title)
+class TitleAdmin(admin.ModelAdmin):
+    """Страница Title в админке"""
+
+    list_display = (
+        'pk',
+        'name',
+        'year',
+        'category',
+    )
+
+    list_filter = ('category', 'genre', 'name', 'year',)
+    list_editable = ('year', 'category')
+    empty_value_display = '-пусто-'
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    """Страница Genre в админке"""
+
+    list_display = (
+        'pk',
+        'name',
+        'slug',
+    )
+
+    list_filter = ('name', 'slug',)
+    empty_value_display = '-пусто-'
